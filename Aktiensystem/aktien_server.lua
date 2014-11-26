8ad5cc241d8130415fa7996a614485ca25fc7c13
MulTi96 <multi1996@yahoo.de>
2014-11-26 17:58:11 +0100

    Update aktien_server.lua

--- Aktiensystem/aktien_server.lua
+++ Aktiensystem/aktien_server.lua
@@ -1,24 +1,66 @@
--- Aktien-System [Server] --
+-- Aktien-System[Server] --
 
 
-GoogleAktien     = {}
-SonyAktien       = {}
-FacebookAktien   = {}
-YoutubeAktien  	 = {}
-VolkswagenAktien = {}
+Aktien = {
+	{ GoogleAktien   = { searchname = "Google" } },
+	{ FacebookAktien = { searchname = "Facebook" } }
+}
+
+AktienVars = {}
+AktienVars["Google"]   = {}
+AktienVars["Facebook"] = {}
 
 
 function loadAktien()
-	fetchRemote("http://www.google.de/search?q=Google+Aktien&oq=Google+Aktien&aqs=chrome..69i57j0l5.2072j0j7&sourceid=chrome&es_sm=0&ie=UTF-8", function(data, errno)
-		if(errno == 0)then
-			local googleAktienFile = fileCreate("googleAktien.txt");
-			fileWrite(googleAktienFile, tostring(data));
-			fileClose(googleAktienFile);
+	
+	for pos, table in pairs(Aktien) do 
+		for table_name, data in pairs(table) do 
 			
-			outputChatBox("Google-Aktien Code wurde erfolgreich in die Datei geschrieben!");
-		else
-			outputChatBox("Google-Aktien Fehler: "..errno)
+			fetchRemote("http://d.yimg.com/autoc.finance.yahoo.com/autoc?query="..data.searchname.."&callback=YAHOO.Finance.SymbolSuggest.ssCallback", function(vars, errno)
+				if(errno == 0)then
+				
+					local json = vars;
+					local json = json:gsub('YAHOO.Finance.SymbolSuggest.ssCallback', '');
+					local json = json:sub(2, #json);
+					local json = json:sub(0, #json - 1);
+					
+					local json = fromJSON(json);
+					
+					for _, variable in pairs(json["ResultSet"]["Result"]) do 
+					
+						AktienVars[data.searchname] = {
+							
+								searchname = data.searchname,
+								fullname   = variable["name"],
+								symbol     = variable["symbol"],
+								exchDisp   = variable["exchDisp"]
+							
+						}
+						
+						local tableVars = AktienVars[data.searchname];
+						
+						dataurl   = "http://query.yahooapis.com/v1/public/yql?q=select%20%2a%20from%20yahoo.finance.quotes%20where%20symbol%20in%20%28%22"..tableVars.symbol.."%22%29&format=json&env=store://datatables.org/alltableswithkeys"
+					
+
+						fetchRemote(dataurl, function(callback_data, callback_errno)
+						
+							if(callback_errno == 0)then
+							
+								local json = callback_data;
+								local json = fromJSON(json);
+								
+								json_data = json["query"]["results"]["quote"];
+								
+								outputChatBox("Aktien Preis von "..tableVars.fullname..": "..json_data["LastTradePriceOnly"]);		
+								
+							end
+						end, "", false, getRootElement());
+					end
+				end
+			end, "", false, getRootElement())
 		end
-	end, "", false, getRootElement())
+	end
 end
 loadAktien();
+
+

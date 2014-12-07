b079c6d42e30a7b46f7a3345069c579a9940bbc2
MulTi96 <multi1996@yahoo.de>
2014-12-07 04:00:14 +0100

    Update sMain.lua

--- Enemys/Funktionen/sMain.lua
+++ Enemys/Funktionen/sMain.lua
@@ -1,1 +1,10 @@
--- Comming Soon --
+function calculateRandomPositionToPlayer(posX, posY, posZ)
+	
+	rndX = math.random(1, 3);
+	rndY = math.random(1, 3);
+	
+	posX = posX + rndX;
+	posY = posY + rnxY;
+	
+	return{posX, posY, posZ};
+end
